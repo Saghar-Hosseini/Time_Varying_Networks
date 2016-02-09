9487d2dc2272d95bd4f50a512e89b44c790ef820
Saghar Hosseini <saghar@uw.edu>
2016-02-08 17:34:20 -0800

    removed Codes folder

--- Codes/test.py
+++
@@ -1,8 +0,0 @@
-    output_file=path+'output'+str(t)+'.text'
-    f = open(output_file, 'w')
-    for node in state.keys():
-        np.savez(f, node, state[node])
-    f.seek(0) # Only needed here to simulate closing & reopening file
-    npzfile = np.load(f)
-    npzfile[node]
-    np.savetxt(f, B)
